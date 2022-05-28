b9336b3a64e6f0cd1ac3df9cb2b1adfe7734083d
divyanshtiwari237 <200020049@iitb.ac.in>
2022-05-28 21:15:04 +0530

    corrected spacing and edited README.md

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Repository created to bring context into the project
 
 ## Building the Project
-1. Recursively clone the git module:`git clone --recursive https://github.com/Project-IPS/Collision-Detection.git`
+1. Recursively clone the git module:
+`git clone --recursive https://github.com/Project-IPS/Collision-Detection.git`
 2. Make sure that cmake is installed.
 3. Configure cmake by running `cmake -Bbuild`
 4. Build the executable using the following command `cmake --build build`
--- main/main.cpp
+++ main/main.cpp
@@ -3,11 +3,9 @@
 #include "vec2.hpp"
 int main()
 {
-    
     World collisionWorld;
-   
-    
 
+    // shape-1
     std::vector<vec2> points2;
     points2.push_back(vec2(0.75,0.5));
     points2.push_back(vec2(0.75,-0.5));
@@ -15,14 +13,11 @@ int main()
     points2.push_back(vec2(-0.25,-0.5));
     vec2 center2 = vec2(0.25,0);
     auto rect2 = Rectangle(points2, center2);
-    
-
     collisionWorld.addShapes(rect2);
 
-
+    // shape-2
     std::vector<vec2> points1;
     points1.push_back(vec2(1,1));
-    
     points1.push_back(vec2(1,-1));
     points1.push_back(vec2(-1,1));
     points1.push_back(vec2(-1,-1));
@@ -30,22 +25,15 @@ int main()
     auto rect = Rectangle(points1, center);
     collisionWorld.addShapes(rect);
 
-
+     // shape-3
     std::vector<vec2> points3;
     points3.push_back(vec2(2,2));
-    
     points3.push_back(vec2(2,-2));
     points3.push_back(vec2(-2,2));
     points3.push_back(vec2(-2,-2));
-
     auto rect3 = Rectangle(points3, center);
-
-    
-    
-    
     collisionWorld.addShapes(rect3);
+
+    // compute all collision
     collisionWorld.Compute();
-    
-
-
 }
