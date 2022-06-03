5ebb804511794293aeb9f6c6b1708176068bc2a5
divyanshtiwari237 <200020049@iitb.ac.in>
2022-06-03 21:23:59 +0530

    implemented the functions inside narrowphase.cpp

--- utilities/include/narrowphase.cpp
+++ utilities/include/narrowphase.cpp
@@ -1,11 +1,15 @@
 #ifndef NARROWPHASE_HPP
 #define NARROWPHASE_HPP
 #include<vector>
+#include<cmath>
 #include"shapes.hpp"
-
+#include"vec2.hpp"
 struct Contact
 {
     // stores information about the detected collision contact point
+    vec2 contactPoint;
+    vec2 normal;
+    double penetrationDepth;
 };
 
 struct Collisiondata
@@ -42,15 +46,37 @@ class Checkcollisions
 
     bool pointInRectangle(vec2 centerofcircle, Rectangle rect)
     {
-        // code to detect whether a point is inside the rectangle or not
+        // code to detect whether a point is inside the rectangle or not.Assuming counterclockwise function
+        bool test = true;
+        for( int i = 0; i< 4; i++)
+        {
+            float x1 = rect.points[i].x;
+            float x2 = rect.points[(i+1)%4].x;
+            float y1 = rect.points[i].y;
+            float y2 = rect.points[(i+1)%4].y;
+            float A = -( y2 - y1);
+            float B = x2 - x1;
+            float C = -(A*x1 + B*y1);
+            float D = A*centerofcircle.x + B*centerofcircle.y + C;
+            test = test && (D>=0);
+        }
+        return test;
     }
 
-    bool intersectCircle(Circle c, vec2 A, vec2 B)
+    bool intersectCircle(Circle c, vec2 p1, vec2 p2)
     {
         // code to detect whether a ray intersects a circle or not
+        float A = p2.y - p1.y;
+        float B = p1.x - p2.x;
+        float C = p1.y*(p2.x - p1.x) - p2.x*(p2.y - p1.y);
+
+        return (perpendicularDistance(A, B, C, c.center) <= c.radius);
+    }
+
+    float perpendicularDistance(float A, float B, float C, vec2 point)
+    {
+        float distance = std::abs(A*point.x +B*point.y + C)/(std::abs(std::sqrt(A*A +B*B)));
+        return distance;
     }
 };
-
-
-
 #endif
