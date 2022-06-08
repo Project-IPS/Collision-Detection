bc926a3a068382cbc984cd3e2d47931d5c416af0
divyanshtiwari237 <200020049@iitb.ac.in>
2022-06-08 14:32:44 +0530

    completed collision resolution code

--- utilities/include/resolution.hpp
+++ utilities/include/resolution.hpp
@@ -38,21 +38,34 @@ void findline(float &A, float &B, float &C, vec2& pt1, vec2& pt2)
 void resolveCollision(vec2& currPos, vec2& prevPos, Rectangle& rect, Circle& c)
 {
     vec2 displacement = vec2(currPos.x - prevPos.x, currPos.y - prevPos.y);
-    for ( int i =0; i < 4; i++)
+    std::vector<vec2> correctionDisplacements;
+    for ( int i = 3; i >= 0; i--)
     {
         int k = i;
-        int j = (i+1)/4;
+        int j = (i+1)%4;
         float A, B, C;
         findline(A, B, C, rect.points[k], rect.points[j] );
         if(checksegmentIntersection(c, rect.points[k], rect.points[j]) || !(checkSide(A, B, C, currPos, prevPos)))
         {
             
             float allowedDistance = perpendicularDistance(A, B, C, prevPos) - c.radius;
-            // make calculation for correcting curPoss;
-            
-
-
+            float disDisplacementVector = std::abs(std::sqrt((currPos.x - prevPos.x)*(currPos.x - prevPos.x) + (currPos.y - prevPos.y)*currPos.y - prevPos.y));
+            vec2 displacementVector = vec2((currPos.x - prevPos.x)/disDisplacementVector, (currPos.y - prevPos.y)/disDisplacementVector); //unit vector
+            float disNormalVector = std::abs(std::sqrt((rect.points[k].x - rect.points[k - 1].x)*(rect.points[k].x - rect.points[k - 1].x) + (rect.points[k].y - rect.points[k-1].y)*rect.points[k].y - rect.points[k-1].y));
+            vec2 normalVector = vec2((rect.points[k].x - rect.points[k-1].x)/disNormalVector, (rect.points[k].y - rect.points[k-1].y)/disNormalVector);
+            float discurrVector = std::abs(std::sqrt((rect.points[j].x - rect.points[k].x)*(rect.points[j].x - rect.points[k].x) + (rect.points[j].y - rect.points[k].y)*rect.points[j].y - rect.points[k].y));
+             vec2 currVector = vec2((rect.points[j].x - rect.points[k].x)/disCurrVector, (rect.points[j].y - rect.points[k].y)/disCurrVector);
+            float cosAngle = displacementVector.x*currVector.x + displacementVector.y*currVector.y;
+            float sinAngle = std::abs(std::sqrt(1 - cosAngle*cosAngle));
+            float travelledDistance = disDisplacementVector*sinAngle;
+            float rollBack = travelledDistance - allowedDistance; // length of the vector which will be used to displace the particle back in correct position
+            vec2 correction =  vec2(normalVector.x*rollBack, normalVector.y*rollBack);
+            correctionDisplacements.push_back(correction);
         }
+    }
+    for(int i =0; i<correctionDisplacements.size(); i++)
+    {
+        currPos = vec2(currPos.x + correctionDisplacements[i].x, currPos.y + correctionDisplacements[i].y );
     }
     
 
