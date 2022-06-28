4d87df8a8a395d26e47dbae2d8e601f77e9dce24
divyanshtiwari237 <200020049@iitb.ac.in>
2022-06-28 16:08:35 +0530

    added tests aabbtests.cpp and bvhtests.cpp

--- testing/aabbtests.cpp
+++ testing/aabbtests.cpp
@@ -6,7 +6,7 @@ TEST_CASE( "aabbs", "[aabb]" ) {
     vec2 max = vec2(1.0, 1.0);
     AABB testaabb = AABB(min, max);
 
-    REQUIRE(testaabb.min.x == min.x + 1);
+    REQUIRE(testaabb.min.x == min.x );
     REQUIRE(testaabb.min.y == min.y); 
     
 }
---
+++ testing/bvhtests.cpp
@@ -0,0 +1,44 @@
+#include <catch2/catch_test_macros.hpp>
+#include"world.hpp"
+#include"aabb.hpp"
+#include"shapes.hpp"
+#include"vec2.hpp"
+#include"bvhnode.hpp"
+#include"bvh.hpp"
+#include"print.hpp"
+
+TEST_CASE( "bvh", "[bvh]" ) {
+    World collisionWorld;
+
+    std::vector<vec2> points1;
+    points1.push_back(vec2(-1,-1));
+    points1.push_back(vec2(1,-1));
+    points1.push_back(vec2(1,1));
+    points1.push_back(vec2(-1,1));
+    vec2 center = vec2(0,0);
+    auto rect = Rectangle(points1, center);
+    collisionWorld.addShapes(rect);
+
+    std::vector<vec2> points2;
+    points2.push_back(vec2(0.75,0.5));
+    points2.push_back(vec2(0.75,-0.5));
+    points2.push_back(vec2(-0.25,0.5));
+    points2.push_back(vec2(-0.25,-0.5));
+    vec2 center2 = vec2(0.25,0);
+    auto rect2 = Rectangle(points2, center2);
+    collisionWorld.addShapes(rect2);
+
+    auto shapes = collisionWorld.shapes;
+    BVH bvh = BVH(shapes);
+    bvh.buildTree(bvh.root, 0, shapes.size() - 1);
+    
+    
+    Rectangle r;
+
+
+    REQUIRE(bvh.root != nullptr);
+    printbuildtree(bvh.root);
+    
+    
+}
+
